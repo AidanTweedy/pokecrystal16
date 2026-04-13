f8ff5e0573deb8eedd40b113486cc825d9c22e48
Aidan Tweedy <adtweedy@gmail.com>
2026-04-12 21:23:19 -0400

    add mudkip and torchic lines

---
+++ data/pokemon/dex_entries/marshtomp.asm
@@ -0,0 +1,10 @@
+	db "MUD FISH@" ; species name
+	dw 204, 617 ; height, weight
+
+	db   "Living on muddy"
+	next "ground that"
+	next "provides poor"
+
+	page "footing has"
+	next "made its legs"
+	next "sturdy.@"
